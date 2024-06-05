8c36931ed5ab6505b630d89e69ccbb22e5b26d65
matheuss.fernandes <matheusfer33@hotmail.com>
2024-06-05 14:51:26 -0300

    changed update_param accepted types from str only to Union[str, bool]

--- src/profiles/schemas/update.py
+++ src/profiles/schemas/update.py
@@ -1,7 +1,8 @@
 from pydantic import BaseModel
+from typing import Union
 
 
 class ProfileUpdate(BaseModel):
     profile_id: str
     update_field: str
-    update_param: str
+    update_param: Union[str, bool]
