ba6d809b124bc0b8fdfc1cf53d5705b963f42f34
matheuss.fernandes <matheusfer33@hotmail.com>
2024-06-05 14:49:53 -0300

    minor code refactor

--- tests/profile_tests/test_profile_validation.py
+++ tests/profile_tests/test_profile_validation.py
@@ -1,18 +1,18 @@
 import pytest
-from hamcrest import assert_that, equal_to, is_, none
+from hamcrest import assert_that, equal_to
 
 from src.profiles.services.profile_validation import ProfileNameValidationService
 from src.profiles.services.profile_validation import ProfileUpdateValidationService
 from src.exeptions.custom_exeptions import BadRequestException
 
 
-def test_profile_valid_name_validator_pass(make_profile_schema):
+def test_profile_valid_name_validator(make_profile_schema):
     profile_pass = make_profile_schema()
     validate = ProfileNameValidationService(profile_pass.profile_name)
-    assert_that(validate.validate_profile_name(), is_(none()))
+    assert_that(validate.validate_profile_name(), equal_to(True))
 
 
-def test_profile_invalid_name_validator_pass(make_profile_schema):
+def test_profile_invalid_name_validator(make_profile_schema):
     profile_fail = make_profile_schema(profile_name=" Inv@lid pr#file n4me")
     validate = ProfileNameValidationService(profile_fail.profile_name)
 
@@ -20,13 +20,13 @@ def test_profile_invalid_name_validator_pass(make_profile_schema):
         validate.validate_profile_name()
 
 
-def test_profile_valid_update_fields_validation_pass(make_profile_update_schema):
+def test_profile_valid_update_fields_validation(make_profile_update_schema):
     profile_pass = make_profile_update_schema()
     validate = ProfileUpdateValidationService(profile_pass)
-    assert_that(validate.profile_update_field_validator(), is_(none()))
+    assert_that(validate.profile_update_field_validator(), equal_to(True))
 
 
-def test_profile_invalid_update_fields_validation_pass(make_profile_update_schema):
+def test_profile_invalid_update_fields_validation(make_profile_update_schema):
     profile_fail = make_profile_update_schema(update_field="non ecxiste")
     validate = ProfileUpdateValidationService(profile_fail)
     assert_that(validate.profile_update_field_validator(), equal_to(False))
