80925c9a7ab492b238e57fa0b85d10943c3bce83
matheuss.fernandes <matheusfer33@hotmail.com>
2024-06-06 17:30:51 -0300

    Added DuplicateDataException to prevent duplicate data from being inserted into the database

--- src/exeptions/custom_exeptions.py
+++ src/exeptions/custom_exeptions.py
@@ -1,4 +1,8 @@
-
 class BadRequestException(Exception):
     def __init__(self, message):
         super().__init__(message)
+
+
+class DuplicateDataException(Exception):
+    def __init__(self, messsage):
+        super().__init__(messsage)
