d05bc013a5310b6e0181c2bae3be9c18a4653eca
matheuss.fernandes <matheusfer33@hotmail.com>
2024-06-06 17:48:11 -0300

    minor code refactor

--- tests/user_tests/test_user_validation.py
+++ tests/user_tests/test_user_validation.py
@@ -16,70 +16,70 @@ def test_user_validator_format_user_schema_spacing(make_user_schema):
     assert_that(validate.format_spacing(), equal_to(user_good))
 
 
-def test_user_validator_contains_invalid_characters_pass(make_user_schema):
-    user = make_user_schema()
-    validate = UserValidatorService(user)
+def test_user_validator_contains_invalid_characters(make_user_schema):
+    user_pass = make_user_schema()
+    validate = UserValidatorService(user_pass)
     assert_that(validate.validate_invalid_chars(), equal_to(False))
 
 
-def test_user_validator_validates_email_format_pass(make_user_schema):
-    user = make_user_schema()
-    validate = UserValidatorService(user)
+def test_user_validator_validates_correct_email_format(make_user_schema):
+    user_pass = make_user_schema()
+    validate = UserValidatorService(user_pass)
     assert_that(validate.validate_email_format(), equal_to(True))
 
 
-def test_user_validator_validates_password_format_pass(make_user_schema):
-    user = make_user_schema()
-    validate = UserValidatorService(user)
+def test_user_validator_validates_correct_password_format(make_user_schema):
+    user_pass = make_user_schema()
+    validate = UserValidatorService(user_pass)
     assert_that(validate.validate_password_format(), equal_to(True))
 
 
-def test_user_validator_validates_language_selected_pass(make_user_schema):
-    user_options = make_user_schema()
-    validate = UserValidatorService(user_options)
+def test_user_validator_validates_correct_language_selected(make_user_schema):
+    user_options_pass = make_user_schema()
+    validate = UserValidatorService(user_options_pass)
     assert_that(validate.validate_language(), equal_to(True))
 
 
-def test_user_validator_validates_currency_selected_pass(make_user_schema):
-    user_options = make_user_schema()
-    validate = UserValidatorService(user_options)
+def test_user_validator_validates_correct_currency_selected(make_user_schema):
+    user_options_pass = make_user_schema()
+    validate = UserValidatorService(user_options_pass)
     assert_that(validate.validate_currency(), equal_to(True))
 
 
-def test_user_validator_validates_country_selected_pass(make_user_schema):
-    user_options = make_user_schema()
-    validate = UserValidatorService(user_options)
+def test_user_validator_validates_correct_country_selected(make_user_schema):
+    user_options_pass = make_user_schema()
+    validate = UserValidatorService(user_options_pass)
     assert_that(validate.validate_country(), equal_to(True))
 
 
-def test_user_validator_validates_speed_unit_selected_pass(make_user_schema):
-    user_options = make_user_schema()
-    validate = UserValidatorService(user_options)
+def test_user_validator_validates_correct_speed_unit_selected(make_user_schema):
+    user_options_pass = make_user_schema()
+    validate = UserValidatorService(user_options_pass)
     assert_that(validate.validate_speed_unit(), equal_to(True))
 
 
-def test_user_validator_validates_volume_unit_selected_pass(make_user_schema):
-    user_options = make_user_schema()
-    validate = UserValidatorService(user_options)
+def test_user_validator_validates_correct_volume_unit_selected(make_user_schema):
+    user_options_pass = make_user_schema()
+    validate = UserValidatorService(user_options_pass)
     assert_that(validate.validate_volume_unit(), equal_to(True))
 
 
-def test_user_validator_validates_length_unit_selected_pass(make_user_schema):
-    user_options = make_user_schema()
-    validate = UserValidatorService(user_options)
+def test_user_validator_validates_correct_length_unit_selected(make_user_schema):
+    user_options_pass = make_user_schema()
+    validate = UserValidatorService(user_options_pass)
     assert_that(validate.validate_length_unit(), equal_to(True))
 
 
-def test_user_validator_validates_temperature_unit_selected_pass(make_user_schema):
-    user_options = make_user_schema()
-    validate = UserValidatorService(user_options)
+def test_user_validator_validates_correct_temperature_unit_selected(make_user_schema):
+    user_options_pass = make_user_schema()
+    validate = UserValidatorService(user_options_pass)
     assert_that(validate.validate_temperature_unit(), equal_to(True))
 
 
-def test_user_validator_validates_user_validation_main_function_pass(make_user_schema):
-    user = make_user_schema()
-    validate = UserValidatorService(user)
-    assert_that(validate.validate_user_schema(), equal_to(True))
+def test_user_validator_validates_correct_user_main_function(make_user_schema):
+    user_pass = make_user_schema()
+    validate = UserValidatorService(user_pass)
+    assert_that(validate.validate_user_schema(), equal_to(None))
 
 
 def test_user_update_validator_correct_validate_user_schema(make_user_update_schema):
