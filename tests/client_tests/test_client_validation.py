61e3a80d0ac60eae3f2cfbf3adff27af0dbfc6b0
matheuss.fernandes <matheusfer33@hotmail.com>
2024-06-10 13:22:11 -0300

    refactored tests for client validation

--- tests/client_tests/test_client_validation.py
+++ tests/client_tests/test_client_validation.py
@@ -1,6 +1,8 @@
 from hamcrest import assert_that, equal_to
 
 from src.clients.services.new_client_validation import ClientValidatorService
+from src.clients.services.cpf_validation import CPFValidationService
+from src.clients.services.cnpj_validation import CNPJValidationService
 
 
 def test_format_spacing_validator(make_client_schema):
@@ -9,53 +11,55 @@ def test_format_spacing_validator(make_client_schema):
                                     cellphone="         +55        (14)991031566         ",
                                     cpf_cnpj="                     464.085.468-46         ")
     client_good = make_client_schema()
-    validate = ClientValidatorService(client_bad)
+    cpf_validation_service = CPFValidationService(client_bad.cpf_cnpj)
+    cnpj_validation_service = CNPJValidationService(client_bad.cpf_cnpj)
+    validate = ClientValidatorService(client_bad, cpf_validation_service, cnpj_validation_service)
     assert_that(validate.format_spacing(), equal_to(client_good))
 
 
-def test_client_validator_not_contains_invalid_chars_pass(make_client_schema):
+def test_client_validator_not_contains_invalid_chars(make_client_schema):
     client_pass = make_client_schema()
-    validate = ClientValidatorService(client_pass)
+    cpf_validation_service = CPFValidationService(client_pass.cpf_cnpj)
+    cnpj_validation_service = CNPJValidationService(client_pass.cpf_cnpj)
+    validate = ClientValidatorService(client_pass, cpf_validation_service, cnpj_validation_service)
     assert_that(validate.validate_invalid_chars(), equal_to(False))
 
 
-def test_client_validator_contains_invalid_chars_name_pass(make_client_schema):
+def test_client_validator_contains_invalid_chars_name(make_client_schema):
     client_fail = make_client_schema(name="     Sta#%@ndard   co#%pany  name   ")
-    validate = ClientValidatorService(client_fail)
+    cpf_validation_service = CPFValidationService(client_fail.cpf_cnpj)
+    cnpj_validation_service = CNPJValidationService(client_fail.cpf_cnpj)
+    validate = ClientValidatorService(client_fail, cpf_validation_service, cnpj_validation_service)
     assert_that(validate.validate_invalid_chars(), equal_to(True))
 
 
-def test_client_validator_contains_invalid_chars_company_name_pass(make_client_schema):
+def test_client_validator_contains_invalid_chars_company_name(make_client_schema):
     client_fail = make_client_schema(company_name="     Sta#%@ndard   co#%pany  name   ")
-    validate = ClientValidatorService(client_fail)
+    cpf_validation_service = CPFValidationService(client_fail.cpf_cnpj)
+    cnpj_validation_service = CNPJValidationService(client_fail.cpf_cnpj)
+    validate = ClientValidatorService(client_fail, cpf_validation_service, cnpj_validation_service)
     assert_that(validate.validate_invalid_chars(), equal_to(True))
 
 
-def test_client_validator_correct_phone_pattern_pass(make_client_schema):
+def test_client_validator_correct_phone_pattern(make_client_schema):
     client_pass = make_client_schema()
-    validate = ClientValidatorService(client_pass)
+    cpf_validation_service = CPFValidationService(client_pass.cpf_cnpj)
+    cnpj_validation_service = CNPJValidationService(client_pass.cpf_cnpj)
+    validate = ClientValidatorService(client_pass, cpf_validation_service, cnpj_validation_service)
     assert_that(validate.validate_cellphone_pattern(), equal_to(True))
 
 
 def test_client_validator_incorrect_phone_pattern_pass(make_client_schema):
     client_fail = make_client_schema(cellphone="+55 (16)555555559")
-    validate = ClientValidatorService(client_fail)
+    cpf_validation_service = CPFValidationService(client_fail.cpf_cnpj)
+    cnpj_validation_service = CNPJValidationService(client_fail.cpf_cnpj)
+    validate = ClientValidatorService(client_fail, cpf_validation_service, cnpj_validation_service)
     assert_that(validate.validate_cellphone_pattern(), equal_to(False))
-
-
-def test_client_validator_correct_cpf_pattern_pass(make_client_schema):
-    client_pass = make_client_schema()
-    validate = ClientValidatorService(client_pass)
-    assert_that(validate.validate_cpf(), equal_to(True))
-
-
-def test_client_validator_incorrect_cpf_pattern_pass(make_client_schema):
-    client_fail = make_client_schema(cpf_cnpj="asdjialsfcjafsçoi")
-    validate = ClientValidatorService(client_fail)
-    assert_that(validate.validate_cpf(), equal_to(False))
 
 
 def test_client_validator_validate_all_function_pass(make_client_schema):
     client = make_client_schema()
-    validate = ClientValidatorService(client)
-    assert_that(validate.is_valid(), equal_to(True))
+    cpf_validation_service = CPFValidationService(client.cpf_cnpj)
+    cnpj_validation_service = CNPJValidationService(client.cpf_cnpj)
+    validate = ClientValidatorService(client, cpf_validation_service, cnpj_validation_service)
+    assert_that(validate.validate_new_client_schema(), equal_to(True))
