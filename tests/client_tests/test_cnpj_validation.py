438136a513983bf7ad28f6d2d24c65287ceb7435
matheuss.fernandes <matheusfer33@hotmail.com>
2024-06-10 13:23:00 -0300

    refactored tests for the cpf and cnpj validation

--- tests/client_tests/test_cnpj_validation.py
+++ tests/client_tests/test_cnpj_validation.py
@@ -1,15 +1,21 @@
 from hamcrest import assert_that, equal_to
 
-from src.clients.services.new_client_validation import CNPJValidator
+from src.clients.services.cnpj_validation import CNPJValidationService
 
 
-def test_cnpj_validator_valid_cnpj_digits_pass(make_client_schema):
+def test_cnpj_validator_valid_cnpj_digits(make_client_schema):
     client_pass = make_client_schema(cpf_cnpj="11.222.333/0001-81")
-    validate = CNPJValidator(client_pass.cpf_cnpj)
+    validate = CNPJValidationService(client_pass.cpf_cnpj)
     assert_that(validate.validate_digits(), equal_to(True))
 
 
-def test_cnpj_validator_invalid_cnpj_digits_pass(make_client_schema):
-    client_pass = make_client_schema(cpf_cnpj="11.222.333/0001-56")
-    validate = CNPJValidator(client_pass.cpf_cnpj)
+def test_cnpj_validator_invalid_cnpj_digits(make_client_schema):
+    client_fail = make_client_schema(cpf_cnpj="11.222.333/0001-56")
+    validate = CNPJValidationService(client_fail.cpf_cnpj)
     assert_that(validate.validate_digits(), equal_to(False))
+
+
+def test_cnpj_validator_invalid_cnpj_pattern(make_client_schema):
+    client_fail = make_client_schema(cpf_cnpj="111.222.333/12222-87")
+    validate = CNPJValidationService(client_fail.cpf_cnpj)
+    assert_that(validate.validate_pattern(), equal_to(False))
--- tests/client_tests/test_cpf_validation.py
+++ tests/client_tests/test_cpf_validation.py
@@ -1,15 +1,21 @@
 from hamcrest import assert_that, equal_to
 
-from src.clients.services.new_client_validation import CPFValidator
+from src.clients.services.cpf_validation import CPFValidationService
 
 
 def test_cpf_validator_valid_cpf_digits_pass(make_client_schema):
     client_pass = make_client_schema()
-    validate = CPFValidator(client_pass.cpf_cnpj)
+    validate = CPFValidationService(client_pass.cpf_cnpj)
     assert_that(validate.validate_digits(), equal_to(True))
 
 
 def test_cpf_validator_invalid_cpf_digits_pass(make_client_schema):
     client_fail = make_client_schema(cpf_cnpj="123.456.789-10")
-    validate = CPFValidator(client_fail.cpf_cnpj)
+    validate = CPFValidationService(client_fail.cpf_cnpj)
     assert_that(validate.validate_digits(), equal_to(False))
+
+
+def test_cnpj_validator_invalid_cpf_pattern(make_client_schema):
+    client_fail = make_client_schema(cpf_cnpj="555.555.7895-98")
+    validate = CPFValidationService(client_fail.cpf_cnpj)
+    assert_that(validate.validate_pattern(), equal_to(False))
